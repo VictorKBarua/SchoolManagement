a2d1bcd06b715113fc57f1bcdde68401ae5b0ef6
Victor Kumar Barua <victor.tts@hotmail.com>
2024-07-17 13:14:13 +0200

    Adjust the Domain properties

--- SchoolManagement_Domain/Student.cs
+++ SchoolManagement_Domain/Student.cs
@@ -11,27 +11,27 @@ namespace SchoolManagement.Domain
     public class Student : BaseEntity
     {
         [MaxLength(50, ErrorMessage = "FirstName should be less then 50 character.")]
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
         [MaxLength(50, ErrorMessage = "LastName should be less then 50 character.")]
-        public string LastName { get; set; }
+        public string? LastName { get; set; }
 
         [MaxLength(10, ErrorMessage = "LastName should be less then 50 character.")]
-        public string Gender { get; set; }
+        public string? Gender { get; set; }
 
         [MaxLength(50, ErrorMessage = "FatherName should be less then 50 character.")]
-        public string FatherName { get; set; }
+        public string? FatherName { get; set; }
 
         [MaxLength(50, ErrorMessage = "MotherName should be less then 50 character.")]
-        public string MotherName { get; set; }
+        public string? MotherName { get; set; }
 
         [MaxLength(50, ErrorMessage = "Address should be less then 50 character.")]
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
         [MaxLength(50, ErrorMessage = "City should be less then 50 character.")]
-        public string City { get; set; }
+        public string? City { get; set; }
 
         [MaxLength(50, ErrorMessage = "Country should be less then 50 character.")]
-        public string Country { get; set; }
+        public string? Country { get; set; }
         public Int64 Phone { get; set; }
         public char Status { get; set; } = '1';
 
